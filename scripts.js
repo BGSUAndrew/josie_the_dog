b927e9adb3c98b7278b5246dfd31f35cda359450
Andrew O'Connor <andrew.oconnor.12@gmail.com>
2023-11-19 15:11:01 -0800

    Josie the dog
    First commit for Josie's site.

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function openImage() {
+    document.getElementById("lightbox").style.display="block";
+}
+
+function closeImage() {
+    document.getElementById("lightbox").style.display="none";
+}
+
+function image_001() {
+    document.getElementById("lightbox").style.display="block";
+    document.getElementById("image_001").style.display="block";
+    document.getElementById("image_002").style.display="none";
+}
